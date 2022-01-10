f9d96f37e05fdfba657342be10f8ce707b03ba6b
PebriKurniaSandi <fksandi07@gmail.com>
2022-01-10 23:37:28 +0700

    Add project files.

---
+++ “WCFService_2Way_20190140111/IClientCallback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace _WCFService_2Way_20190140111
+{
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+    }
+}
---
+++ “WCFService_2Way_20190140111/IServiceCallback.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace _WCFService_2Way_20190140111
+{
+    
+    [ServiceContract]
+    public interface IServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+    }
+}
